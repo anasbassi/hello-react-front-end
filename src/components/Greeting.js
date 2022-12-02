cde26dfee9d65ad568d6513c467d951c98fdd858
Anas Abubakar <anasabubakar2015@gmail.com>
2022-12-02 16:44:50 +0100

    Add required changes

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchGreeting } from '../store/greetings';
 
@@ -9,7 +8,7 @@ const Greeting = () => {
 
   useEffect(() => {
     dispatch(fetchGreeting());
-  });
+  }, [dispatch]);
 
   const handleGreeting = () => {
     dispatch(fetchGreeting());
@@ -21,7 +20,7 @@ const Greeting = () => {
         Greetings:
         {greet}
       </h1>
-      <button onClick={handleGreeting} type="submit">Greet Randomly</button>
+      <button onClick={(e) => handleGreeting(e)} type="submit">Greet Randomly</button>
     </div>
   );
 };
